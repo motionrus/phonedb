8f2fbc27ca2c1c3fa7aee87e2f47da0b9c2abf89
Ruslan Tyutin <tytin@narod.ru>
2018-01-11 18:24:37 +0300

    my db

--- apps/phone/models.py
+++ apps/phone/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class InnerNumber(models.Model):
+    TYPE_OF_ACCESS = (
+        ('no', 'нет'),
+        ('DTMF', 'донабор'),
+        ('direct number', 'прямой номер'),
+    )
+    STATUS = (
+        ('1', 'enabled'),
+        ('0', 'disabled'),
+    )
+    active = models.CharField(max_length=10,
+                              choices=STATUS,
+                              default='0')
+    inner_number = models.CharField(max_length=10)
+    inner_number_passwd = models.CharField(max_length=20)
+    customer = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    calling_line_id = models.CharField(max_length=11)
+    access_to_MG = models.CharField(max_length=10,
+                                    choices=TYPE_OF_ACCESS,
+                                    default='no')
+    device = models.CharField(max_length=20)
+    login_device = models.CharField(max_length=20)
+    pass_device = models.CharField(max_length=20)
+    wan = models.GenericIPAddressField(protocol='IPv4')
+    lan = models.GenericIPAddressField(protocol='IPv4')
+    data_create = models.DateField(auto_now=True)
+    description = models.DateField(max_length=255)
+
+
+class ExtraNumber(models.Model):
+    pass
