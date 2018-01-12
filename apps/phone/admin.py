6ad9e1e3fd2a3e0ec908243810f60b4553f40800
Ruslan Tyutin <tytin@narod.ru>
2018-01-12 17:22:34 +0300

    my_models

--- apps/phone/admin.py
+++ apps/phone/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ExternalNumber, InternalNumber, Client
+
+
+class InternalNumberAdmin(admin.StackedInline):
+    model = InternalNumber
+    extra = 1
+
+class CustomerClient(admin.StackedInline):
+    model = ExternalNumber
+    extra = 1
+
+class ExternalNumberAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['calling_line',
+                           'type_of_access',
+                           ]}),
+    ]
+
+    inlines = [InternalNumberAdmin]
+
+
+admin.site.register(ExternalNumber, ExternalNumberAdmin)
--- apps/phone/models.py
+++ apps/phone/models.py
@@ -1,36 +1,59 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
 
-class InnerNumber(models.Model):
+
+class ExternalNumber(models.Model):
+    TYPE_OF_ACCESS = (
+        ('any', 'общий'),
+        ('specific', 'выделенный'),
+    )
+    calling_line = models.CharField(max_length=11, unique=True)
+    type_of_access = models.CharField(max_length=10,
+                                      choices=TYPE_OF_ACCESS)
+
+    def __str__(self):
+        return self.calling_line
+
+
+class InternalNumber(models.Model):
     TYPE_OF_ACCESS = (
         ('no', 'нет'),
         ('DTMF', 'донабор'),
         ('direct number', 'прямой номер'),
     )
+    access_to_MG = models.CharField(max_length=20,
+                                    choices=TYPE_OF_ACCESS,
+                                    default='no')
+    calling_line_id = models.ForeignKey(ExternalNumber,
+                                        on_delete=models.SET_NULL,
+                                        blank=True,
+                                        null=True)
+    inner_number = models.CharField(max_length=10)
+    inner_number_passwd = models.CharField(max_length=20 )
+    device = models.CharField(max_length=20)
+    login_device = models.CharField(max_length=20)
+    pass_device = models.CharField('Пароль устройства', max_length=20)
+    wan = models.GenericIPAddressField(protocol='IPv4')
+    lan = models.GenericIPAddressField(protocol='IPv4', blank=True, null=True)
+
+    def __str__(self):
+        return self.inner_number
+
+class Client(models.Model):
     STATUS = (
         ('1', 'enabled'),
         ('0', 'disabled'),
     )
+    phone_number = models.ForeignKey(InternalNumber,
+                                     on_delete=models.CASCADE)
     active = models.CharField(max_length=10,
                               choices=STATUS,
                               default='0')
-    inner_number = models.CharField(max_length=10)
-    inner_number_passwd = models.CharField(max_length=20)
     customer = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    calling_line_id = models.CharField(max_length=11)
-    access_to_MG = models.CharField(max_length=10,
-                                    choices=TYPE_OF_ACCESS,
-                                    default='no')
-    device = models.CharField(max_length=20)
-    login_device = models.CharField(max_length=20)
-    pass_device = models.CharField(max_length=20)
-    wan = models.GenericIPAddressField(protocol='IPv4')
-    lan = models.GenericIPAddressField(protocol='IPv4')
-    data_create = models.DateField(auto_now=True)
-    description = models.DateField(max_length=255)
+    date_last_change = models.DateField(auto_now=True)
+    description = models.CharField(max_length=255, blank=True)
 
-
-class ExtraNumber(models.Model):
-    pass
+    def __str__(self):
+        return self.customer
